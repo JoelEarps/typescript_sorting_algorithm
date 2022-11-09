9a18c443cbf9682cbb558b01b17270892ccb1889
Joel Earps <joel.earps@zenobe.com>
2022-11-09 15:25:23 +0000

    Breaking Change! : Implementing Inheritance

--- build/index.js
+++ build/index.js
@@ -4,6 +4,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter/Sorter");
 const NumbersCollection_1 = require("./Collections/NumbersCollection");
 const CharactersCollection_1 = require("./Collections/CharactersCollection");
+const LinkedList_1 = require("./Collections/LinkedList");
+// Have to create an instance of sorter to extend any method of collection - to get around this you can
+// Use Inheritance - class extends
 const numCol = new NumbersCollection_1.NumbersCollection([5, 100, -3, 46]);
 const charCol = new CharactersCollection_1.CharactersCollection('test');
 const sorter = new Sorter_1.Sorter(numCol);
@@ -12,3 +15,9 @@ charSorter.sort();
 sorter.sort();
 console.log(numCol.data);
 console.log(charCol.data);
+const LL = new LinkedList_1.LinkedList();
+LL.add(500);
+LL.add(-10), LL.add(0);
+const LLSorter = new Sorter_1.Sorter(LL);
+LLSorter.sort();
+LL.print();
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,10 @@
 import { Sorter } from './Sorter/Sorter';
 import { NumbersCollection } from './Collections/NumbersCollection';
 import { CharactersCollection } from './Collections/CharactersCollection';
+import { LinkedList } from './Collections/LinkedList';
 
+// Have to create an instance of sorter to extend any method of collection - to get around this you can
+// Use Inheritance - class extends
 const numCol = new NumbersCollection([5, 100, -3, 46]);
 const charCol = new CharactersCollection('test');
 const sorter = new Sorter(numCol);
@@ -11,3 +14,10 @@ charSorter.sort();
 sorter.sort();
 console.log(numCol.data);
 console.log(charCol.data);
+
+const LL = new LinkedList();
+LL.add(500);
+LL.add(-10), LL.add(0);
+const LLSorter = new Sorter(LL);
+LLSorter.sort();
+LL.print();
